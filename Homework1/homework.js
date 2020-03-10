d6ba24fff42b36909922c8a3cb676edb2224eaeb
Andrii Kuzma <51418233+andkuzma@users.noreply.github.com>
2020-03-10 21:59:41 +0200

    Homework 1

---
+++ Homework1/homework.js
@@ -0,0 +1,74 @@
+// 2 Task
+// alert("My name is Andrii Kuzma");
+
+
+// 3 Task
+// let x = 50;
+// let name = "Ivan";
+// alert(x);
+// alert(name);
+// x = name;
+// alert(x);
+// alert(name);
+
+
+// 4 Task
+// let x;
+// var user = {
+//     name: "Andrii", 
+//     age: 25,
+//     married: true,
+//     nullVariable: null,
+//     undefVariable: x
+//  };
+//  console.log(user);
+
+
+
+// 5 Task
+//  let isAdult= confirm("Are you an adult?");
+//  alert(isAdult);
+
+
+
+
+// 6 Task
+// let firstName = "Andrii";
+// let lastName = "Kuzma";
+// let Group = "Lv-494.JS core";
+// let yearBirthday = 1994;
+// let married = false;
+
+// console.log(typeof yearBirthday); 
+// console.log(typeof married); 
+// console.log(typeof firstName); 
+// console.log(typeof lastName);
+// console.log(typeof Group);  
+
+// let age;
+// let z = null;
+// console.log(typeof age); 
+// console.log(typeof z); 
+
+
+
+// 7 Task
+// let username = prompt("Please enter your name", "Ivan");
+// let email = prompt("Please enter your email", "email@gmail.com");
+// let pass = prompt("Please enter your password", "qwerty");
+
+// alert(`Dear ${username}, your email is ${email}, your password is ${pass}.` ); 
+
+
+// 8 Task
+
+// let hour = 60*60;
+// let day = hour*24;
+// let month = day*31;
+
+// alert(`In 1 hour ${hour} seconds`);
+// alert(`In 1 day ${day} seconds`);
+// alert(`In 1 month ${month} seconds`);
+
+
+
